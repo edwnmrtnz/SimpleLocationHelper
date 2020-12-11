41bc7157e7ea73d568f1745337dd42a56572b391
Edwin Martinez <dev.edwinmartinez@gmail.com>
2020-12-11 12:41:56 +0800

    Remove gps status listener and add ability to destroy instance to avoid leaks

--- library/src/main/java/com/edwnmrtnz/locationprovider/LocationProviderHelper.java
+++ library/src/main/java/com/edwnmrtnz/locationprovider/LocationProviderHelper.java
@@ -3,7 +3,6 @@ package com.edwnmrtnz.locationprovider;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
@@ -15,7 +14,6 @@ import androidx.core.app.ActivityCompat;
 
 import com.edwnmrtnz.locationprovider.callback.OnLocationReceiver;
 import com.edwnmrtnz.locationprovider.enums.LocationUpdateStatus;
-import com.edwnmrtnz.locationprovider.receiver.GpsStatusChangeReceiver;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -32,7 +30,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 
-public class LocationProviderHelper implements GpsStatusChangeReceiver.OnGpsLocationSettingsChanged {
+public class LocationProviderHelper {
     private static final String TAG = "LocationProviderHelper";
 
     private static LocationProviderHelper INSTANCE = null;
@@ -42,8 +40,8 @@ public class LocationProviderHelper implements GpsStatusChangeReceiver.OnGpsLoca
     private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 2000;
     private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2;
 
-    private FusedLocationProviderClient fusedLocationProviderClient;
-    private SettingsClient settingsClient;
+    private final FusedLocationProviderClient fusedLocationProviderClient;
+    private final SettingsClient settingsClient;
     private LocationRequest locationRequest;
     private LocationSettingsRequest locationSettingsRequest;
     private LocationCallback locationCallback;
@@ -51,12 +49,8 @@ public class LocationProviderHelper implements GpsStatusChangeReceiver.OnGpsLoca
     private OnLocationReceiver onLocationReceiver;
     private Location bestLastKnownLocation;
 
-    private GpsStatusChangeReceiver gpsStatusChangeReceiver;
-
     public LocationProviderHelper(Context context) {
         this.context = context;
-
-        gpsStatusChangeReceiver = new GpsStatusChangeReceiver();
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context);
 
@@ -70,8 +64,8 @@ public class LocationProviderHelper implements GpsStatusChangeReceiver.OnGpsLoca
     }
 
     public static LocationProviderHelper getInstance(Context context) {
-        if(context == null) throw new NullPointerException("Context cannot be null");
-        if(INSTANCE == null) INSTANCE = new LocationProviderHelper(context);
+        if (context == null) throw new NullPointerException("Context cannot be null");
+        if (INSTANCE == null) INSTANCE = new LocationProviderHelper(context);
         return INSTANCE;
     }
 
@@ -110,31 +104,18 @@ public class LocationProviderHelper implements GpsStatusChangeReceiver.OnGpsLoca
     }
 
     public void startLocationUpdates() {
-
-        startGpsTracking();
-
         settingsClient.checkLocationSettings(locationSettingsRequest)
                 .addOnSuccessListener((Activity) context, new OnSuccessListener<LocationSettingsResponse>() {
                     @Override
                     public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
-
-                        Log.e(TAG, "Should start location update");
-
                         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                            Log.e(TAG, "Permission not permitted");
-                            if(onLocationReceiver != null) onLocationReceiver.onFailed(LocationUpdateStatus.NO_PERMISSION);
+                            if (onLocationReceiver != null)
+                                onLocationReceiver.onFailed(LocationUpdateStatus.NO_PERMISSION);
                             return;
                         }
                         fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
-                        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-                            @Override
-                            public void onSuccess(Location location) {
-                                if(location != null) {
-                                    Log.e(TAG, "Last known location: " + location.toString());
-                                }
-                            }
-                        });
-                        if(onLocationReceiver != null) onLocationReceiver.onLocationReceiverStarted();
+                        if (onLocationReceiver != null)
+                            onLocationReceiver.onLocationReceiverStarted();
 
                     }
                 }).addOnFailureListener((Activity) context, new OnFailureListener() {
@@ -142,14 +123,15 @@ public class LocationProviderHelper implements GpsStatusChangeReceiver.OnGpsLoca
             public void onFailure(@NonNull Exception e) {
                 int statusCode = ((ApiException) e).getStatusCode();
                 switch (statusCode) {
-                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:{
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED: {
                         Log.e(TAG, "Device has problem that can be solved by following the message in the exception: " + e.getMessage());
-                        if(onLocationReceiver != null) onLocationReceiver.onResolutionRequired(e);
+                        if (onLocationReceiver != null) onLocationReceiver.onResolutionRequired(e);
                         break;
                     }
-                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:{
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE: {
                         Log.e(TAG, "Device has problem that can't be solved");
-                        if(onLocationReceiver != null) onLocationReceiver.onFailed(LocationUpdateStatus.NOT_RESOLVABLE);
+                        if (onLocationReceiver != null)
+                            onLocationReceiver.onFailed(LocationUpdateStatus.NOT_RESOLVABLE);
                         break;
                     }
                 }
@@ -157,25 +139,15 @@ public class LocationProviderHelper implements GpsStatusChangeReceiver.OnGpsLoca
         });
     }
 
-    private void startGpsTracking() {
-        context.registerReceiver(gpsStatusChangeReceiver, new IntentFilter(LocationManager.PROVIDERS_CHANGED_ACTION));
-        gpsStatusChangeReceiver.setOnGpsLocationSettingsChanged(this);
-    }
-
-    private void stopGpsTracking() {
-        context.unregisterReceiver(gpsStatusChangeReceiver);
-    }
-
     private void sendUpdates() {
-        if(onLocationReceiver != null) onLocationReceiver.onLocationAcquired(bestLastKnownLocation, bestLastKnownLocation.getAccuracy());
+        if (onLocationReceiver != null)
+            onLocationReceiver.onLocationAcquired(bestLastKnownLocation, bestLastKnownLocation.getAccuracy());
     }
 
     public void stopLocationUpdates() {
-
-        stopGpsTracking();
-
-        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
-                .addOnCompleteListener((Activity)context, new OnCompleteListener<Void>() {
+        fusedLocationProviderClient
+                .removeLocationUpdates(locationCallback)
+                .addOnCompleteListener((Activity) context, new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         Log.e(TAG, "On Location updates has been stop via stopLocationUpdates()");
@@ -183,16 +155,13 @@ public class LocationProviderHelper implements GpsStatusChangeReceiver.OnGpsLoca
                 });
     }
 
-    public boolean isLocationEnabled() {
-        LocationManager location = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
-        return location.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    void destroyInstance() {
+        context = null;
+        INSTANCE = null;
     }
 
-    @Override
-    public void onGpsSettingsChanged() {
-        if(!isLocationEnabled()) {
-            if(onLocationReceiver != null) onLocationReceiver.onFailed(LocationUpdateStatus.GPS_NOT_OPEN);
-            startLocationUpdates();
-        }
+    public boolean isLocationEnabled() {
+        LocationManager location = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        return location.isProviderEnabled(LocationManager.GPS_PROVIDER);
     }
 }
--- library/src/main/java/com/edwnmrtnz/locationprovider/enums/LocationUpdateStatus.java
+++ library/src/main/java/com/edwnmrtnz/locationprovider/enums/LocationUpdateStatus.java
@@ -1,5 +1,5 @@
 package com.edwnmrtnz.locationprovider.enums;
 
 public enum LocationUpdateStatus {
-    NO_PERMISSION, GPS_NOT_OPEN, NOT_RESOLVABLE
+    NO_PERMISSION, NOT_RESOLVABLE
 }
